eeb75a1843eb9388b460aece1d295a434c2302e2
sotoslammer <desjarlais.brendan@gmail.com>
2021-03-13 01:15:03 -0600

    get the backend working

---
+++ backend/src/server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {postgraphile} from "postgraphile"
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const {PORT, DB_HOST, POSTGRES_DB, DATABASE_URL, POSTGRES_USER, POSTGRES_PASSWORD, HOST, PG_PORT} = process.env
+
+const host = HOST || "localhost";
+const dbHost = DB_HOST ?? "localhost";
+const port = parseInt(PORT ?? "8080");
+const dbPort = parseInt(PG_PORT ?? "5432");
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(postgraphile(
+  DATABASE_URL ?? {
+      database: POSTGRES_DB,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      host: dbHost,
+      port: dbPort,
+  }, "public", {
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+  }));
+
+app.listen(port,() => {
+    return console.log(`server is listening on ${port}`);
+});
